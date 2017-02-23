9ae86e8beafe334ccb449288bc8a7627ffc66305
Emma Tapia <clioemma_28@hotmail.com>
2017-02-23 10:16:16 -0500

    Projecto3Celsius1

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -2,13 +2,18 @@
 function convertirCelsius(){
 	var element = document.getElementById("farenheit").value;
 	var celsius = ((element-32)*(5/9));
-
+	 if(element !== ""){
+	
 	document.getElementById("celsius").value = celsius.toFixed(2);
+	}
 		}
 
 function convertirFarenheit(){
 	var element = document.getElementById("celsius").value;
 	var farenheit = ((9*element)/5)+32;
 
+	if(element!== ""){
+
 	document.getElementById("farenheit").value = farenheit.toFixed(2);
+	}
 		}
